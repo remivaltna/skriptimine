124727280e8ccb73bba2d79c9e58a32ec4a4c3e6
remi Valtna <remi.valtna@khk.ee>
2019-05-06 13:46:17 +0300

    yl3

---
+++ praks4a/yl3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#
+tar -czf /home/user/skriptimine/praks4a/backup/test.tar.gz
+echo "kataloogi /home/user/skirptimine/praks4a/test backupi nimi on test.tar.gz ja see asub /home/user/skirpitimine/praks4a/backup kaustas
+#
+#"
